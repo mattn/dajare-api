dcda13842162381f624cd3e6fc6db88fa26a5b25
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-04-26 23:51:06 +0900

    first import

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
+)
+
+const name = "dajare-api"
+
+const version = "0.0.0"
+
+var revision = "HEAD"
+
+func main() {
+	var dsn string
+	var ver bool
+
+	flag.StringVar(&dsn, "dsn", os.Getenv("DATABASE_URL"), "Database source")
+	flag.BoolVar(&ver, "v", false, "show version")
+	flag.Parse()
+
+	if ver {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		var text string
+		err = db.QueryRow(`SELECT text FROM dajare ORDER BY RANDOM() LIMIT 1`).Scan(&text)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(text)
+		return c.JSON(http.StatusOK, struct {
+			Text string `json:"text"`
+		}{
+			Text: text,
+		})
+	})
+	e.Logger.Fatal(e.Start(":8989"))
+}
